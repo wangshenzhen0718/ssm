9d97f78026bb339a494fcabbfce2683f9af98f19
wangshenzhen0718 <153474100@qq.com>
2022-11-10 22:22:32 +0800

    javassist动态生成类并实现接口中所有方法

--- mybatis/javassist-test/src/test/java/com/wang/javassist/JavassistTest.java
+++ mybatis/javassist-test/src/test/java/com/wang/javassist/JavassistTest.java
@@ -7,8 +7,69 @@ import javassist.CtMethod;
 import org.junit.Test;
 
 import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class JavassistTest {
+    @Test
+    public void testGenerateAccountDaoImpl() throws Exception {
+        //获取类池
+        ClassPool pool = ClassPool.getDefault();
+        // 制造类
+        CtClass ctClass = pool.makeClass("com.wang.bank.dao.impl.AccountDaoImpl");
+        // 制造接口
+        CtClass ctInterface = pool.makeInterface("com.wang.javassist.dao.AccountDao");
+        //实现接口
+        ctClass.addInterface(ctInterface);
+        //实现接口中所有的方法
+        Method[] declaredMethods = AccountDao.class.getDeclaredMethods();
+        System.out.println(declaredMethods.length);
+        //制造方法
+        Arrays.stream(declaredMethods).forEach(method->{
+            try {
+                //int update(String actno,Double balance);
+                StringBuilder methodCode = new StringBuilder();
+                methodCode.append("public");
+                methodCode.append(" ");
+                methodCode.append(method.getReturnType().getName());
+                methodCode.append(" ");
+                methodCode.append(method.getName());
+                methodCode.append("(");
+                Class<?>[] parameterTypes = method.getParameterTypes();
+                for (int i = 0; i < parameterTypes.length; i++) {
+                    Class<?> parameterType = parameterTypes[i];
+                    methodCode.append(parameterType.getName());
+                    methodCode.append(" ");
+                    methodCode.append("arg"+i);
+                    if (i!=parameterTypes.length-1){
+                        methodCode.append(",");
+                    }
+                }
+                methodCode.append("){System.out.println(\"111\");");
+                if ("void".equals(method.getReturnType().getName())){
+                }else if ("int".equals(method.getReturnType().getName())){
+                    methodCode.append("return 1;");
+                }else if("java.lang.String".equals(method.getReturnType().getName())){
+                    methodCode.append("return \"666\";");
+                }
+                methodCode.append("}");
+                System.out.println(methodCode);
+                CtMethod ctMethod = CtMethod.make(String.valueOf(methodCode), ctClass);
+                //讲方法添加到类中
+                ctClass.addMethod(ctMethod);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        });
+        //在内存中生成class，并且加载到JVM当中
+        Class toClass = ctClass.toClass();
+        // 创建对象
+        AccountDao accountDao = (AccountDao) toClass.newInstance();
+        //调用方法
+        accountDao.delete();
+        accountDao.insert("666");
+        accountDao.selectByActno("555");
+        accountDao.update("555",20.0);
+    }
     @Test
     public void generateInterface() throws Exception {
         ClassPool pool = ClassPool.getDefault();
@@ -24,8 +85,6 @@ public class JavassistTest {
         Class aClass = ctClass.toClass();
         AccountDao accountDao = (AccountDao) aClass.newInstance();
         accountDao.delete();
-
-
     }
 
     @Test
--- mybatis/javassist-test/src/test/java/com/wang/javassist/dao/AccountDao.java
+++ mybatis/javassist-test/src/test/java/com/wang/javassist/dao/AccountDao.java
@@ -2,4 +2,8 @@ package com.wang.javassist.dao;
 
 public interface AccountDao {
     void delete();
+    int insert(String actno);
+    int update(String actno,Double balance);
+    String selectByActno(String actno);
+
 }

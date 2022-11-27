cce48c53a58ff3bd62757f3be1f2e0da3e324ab4
wangshenzhen0718 <153474100@qq.com>
2022-11-27 22:58:52 +0800

    Bean的生命周期之5步

---
+++ spring6/spring6-006-lifecycle/src/main/java/com/wang/spring6/bean/User.java
@@ -0,0 +1,28 @@
+package com.wang.spring6.bean;
+/**
+ * @Author:wsz
+ * @Date: 2022/11/27 22:51
+ * @Description:
+ * @Version: 1.0
+ * @Since: 1.0
+ */
+public class User {
+
+    private String name;
+
+    public User() {
+        System.out.println("第一步 构造函数");
+    }
+
+    public void setName(String name) {
+        System.out.println("第二步 set赋值");
+        this.name = name;
+    }
+    public void initBean(){
+        System.out.println("第三步 初始化bean");
+    }
+
+    public void destoryBean(){
+        System.out.println("第五步 销毁bean");
+    }
+}
---
+++ spring6/spring6-006-lifecycle/src/test/java/com/wang/spring6/test/BeanLifecycleTest.java
@@ -0,0 +1,19 @@
+package com.wang.spring6.test;
+
+import com.wang.spring6.bean.User;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanLifecycleTest {
+
+    @Test
+    public void testLifecycle(){
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
+        User userBean = applicationContext.getBean("userBean", User.class);
+        System.out.println("第四步 使用Bean");
+        // 只有正常关闭spring容器才会执行销毁方法
+        ClassPathXmlApplicationContext context = (ClassPathXmlApplicationContext) applicationContext;
+        context.close();
+    }
+}

2cedb085d97a91389dc9127c07a51b104d81c977
wangshenzhen0718 <153474100@qq.com>
2022-12-03 22:43:16 +0800

    JdbcTemplate操作数据库

--- spring6/spring6-009-jdbc/src/test/java/com/wang/spring6/test/JdbcTest.java
+++ spring6/spring6-009-jdbc/src/test/java/com/wang/spring6/test/JdbcTest.java
@@ -1,9 +1,20 @@
 package com.wang.spring6.test;
 
+import com.wang.spring6.bean.User;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCallback;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class JdbcTest {
     @Test
@@ -16,5 +27,147 @@ public class JdbcTest {
         String sql = "insert into t_user(id,real_name,age) values(?,?,?)";
         int count = jdbcTemplate.update(sql, null, "张三", 30);
         System.out.println("插入的记录条数：" + count);
+    }
+
+    @Test
+    public void testUpdate(){
+        // 获取JdbcTemplate对象
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
+        JdbcTemplate jdbcTemplate = applicationContext.getBean("jdbcTemplate", JdbcTemplate.class);
+        // 执行插入操作
+        // 注意：insert delete update的sql语句，都是执行update方法。
+        String sql = "update t_user set real_name=?,age=? where id=?";
+        int count = jdbcTemplate.update(sql, "zhangsan", 30, 1);
+        System.out.println("修改的记录条数：" + count);
+    }
+
+    @Test
+    public void testDelete(){
+        // 获取JdbcTemplate对象
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
+        JdbcTemplate jdbcTemplate = applicationContext.getBean("jdbcTemplate", JdbcTemplate.class);
+        // 执行插入操作
+        // 注意：insert delete update的sql语句，都是执行update方法。
+        String sql = "delete from t_user where id=?";
+        int count = jdbcTemplate.update(sql, 1);
+        System.out.println("删除的记录条数：" + count);
+    }
+
+    @Test
+    public void testSelect(){
+        // 获取JdbcTemplate对象
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
+        JdbcTemplate jdbcTemplate = applicationContext.getBean("jdbcTemplate", JdbcTemplate.class);
+        // 执行插入操作
+        // 注意：insert delete update的sql语句，都是执行update方法。
+        String sql = "select * from t_user where id=?";
+        User user = jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<>(User.class), 2);
+        System.out.println(user);
+    }
+
+    @Test
+    public void testSelectBatch(){
+        // 获取JdbcTemplate对象
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
+        JdbcTemplate jdbcTemplate = applicationContext.getBean("jdbcTemplate", JdbcTemplate.class);
+        // 执行插入操作
+        // 注意：insert delete update的sql语句，都是执行update方法。
+        String sql = "select * from t_user";
+        List<User> users = jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(User.class));
+        System.out.println(users);
+    }
+    @Test
+    public void testQueryOneValue(){
+        // 获取JdbcTemplate对象
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
+        JdbcTemplate jdbcTemplate = applicationContext.getBean("jdbcTemplate", JdbcTemplate.class);
+        // 执行插入操作
+        // 注意：insert delete update的sql语句，都是执行update方法。
+        String sql = "select count(1) from t_user";
+        Integer integer = jdbcTemplate.queryForObject(sql, int.class);
+        System.out.println(integer);
+    }
+
+    @Test
+    public void testAddBatch(){
+        // 获取JdbcTemplate对象
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
+        JdbcTemplate jdbcTemplate = applicationContext.getBean("jdbcTemplate", JdbcTemplate.class);
+        // 批量添加
+        String sql = "insert into t_user(id,real_name,age) values(?,?,?)";
+
+        Object[] objs1 = {null, "小花", 20};
+        Object[] objs2 = {null, "小明", 21};
+        Object[] objs3 = {null, "小刚", 22};
+        List<Object[]> list = new ArrayList<>();
+        list.add(objs1);
+        list.add(objs2);
+        list.add(objs3);
+
+        int[] count = jdbcTemplate.batchUpdate(sql, list);
+        System.out.println(Arrays.toString(count));
+    }
+
+    @Test
+    public void testUpdateBatch(){
+        // 获取JdbcTemplate对象
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
+        JdbcTemplate jdbcTemplate = applicationContext.getBean("jdbcTemplate", JdbcTemplate.class);
+        // 批量修改
+        String sql = "update t_user set real_name = ?, age = ? where id = ?";
+        Object[] objs1 = {"小花11", 10, 2};
+        Object[] objs2 = {"小明22", 12, 3};
+        Object[] objs3 = {"小刚33", 9, 4};
+        List<Object[]> list = new ArrayList<>();
+        list.add(objs1);
+        list.add(objs2);
+        list.add(objs3);
+
+        int[] count = jdbcTemplate.batchUpdate(sql, list);
+        System.out.println(Arrays.toString(count));
+    }
+
+    @Test
+    public void testDeleteBatch(){
+        // 获取JdbcTemplate对象
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
+        JdbcTemplate jdbcTemplate = applicationContext.getBean("jdbcTemplate", JdbcTemplate.class);
+        // 批量删除
+        String sql = "delete from t_user where id = ?";
+        Object[] objs1 = {2};
+        Object[] objs2 = {3};
+        Object[] objs3 = {4};
+        List<Object[]> list = new ArrayList<>();
+        list.add(objs1);
+        list.add(objs2);
+        list.add(objs3);
+        int[] count = jdbcTemplate.batchUpdate(sql, list);
+        System.out.println(Arrays.toString(count));
+    }
+
+
+    @Test
+    public void testCallback(){
+        // 获取JdbcTemplate对象
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
+        JdbcTemplate jdbcTemplate = applicationContext.getBean("jdbcTemplate", JdbcTemplate.class);
+        String sql = "select id, real_name, age from t_user where id = ?";
+
+        User user = jdbcTemplate.execute(sql, new PreparedStatementCallback<User>() {
+            @Override
+            public User doInPreparedStatement(PreparedStatement ps) throws SQLException, DataAccessException {
+                User user = null;
+                ps.setInt(1, 5);
+                ResultSet rs = ps.executeQuery();
+                if (rs.next()) {
+                    user = new User();
+                    user.setId(rs.getInt("id"));
+                    user.setRealName(rs.getString("real_name"));
+                    user.setAge(rs.getInt("age"));
+                }
+                return user;
+            }
+        });
+        System.out.println(user);
     }
 }

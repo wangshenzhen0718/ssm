e35b6ae287fa776bbb0bf05539f3f0b3ce4d5a13
wangshenzhen0718 <153474100@qq.com>
2022-12-07 15:08:21 +0800

    Spring事务模拟转账失败

---
+++ spring6/spring6-013-tx-bank/src/main/java/com/wang/bank/service/impl/AccountServiceImpl.java
@@ -0,0 +1,34 @@
+package com.wang.bank.service.impl;
+
+import com.wang.bank.dao.AccountDao;
+import com.wang.bank.pojo.Account;
+import com.wang.bank.service.AccountService;
+import jakarta.annotation.Resource;
+import org.springframework.stereotype.Service;
+
+@Service("accountService")
+public class AccountServiceImpl implements AccountService {
+
+    @Resource(name = "accountDao")
+    private AccountDao accountDao;
+
+    @Override
+    public void transfer(String fromActno, String toActno, double money) {
+        // 查询账户余额是否充足
+        Account fromAct = accountDao.selectByActno(fromActno);
+        if (fromAct.getBalance() < money) {
+            throw new RuntimeException("账户余额不足");
+        }
+        // 余额充足，开始转账
+        Account toAct = accountDao.selectByActno(toActno);
+        fromAct.setBalance(fromAct.getBalance() - money);
+        toAct.setBalance(toAct.getBalance() + money);
+        int count = accountDao.update(fromAct);
+        String s=null;
+        s.toString();
+        count += accountDao.update(toAct);
+        if (count != 2) {
+            throw new RuntimeException("转账失败，请联系银行");
+        }
+    }
+}
---
+++ spring6/spring6-013-tx-bank/src/test/java/com/wang/bank/test/BankTest.java
@@ -0,0 +1,21 @@
+package com.wang.bank.test;
+
+import com.wang.bank.service.AccountService;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BankTest {
+    @Test
+    public void testTransfer(){
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
+        AccountService accountService = applicationContext.getBean("accountService", AccountService.class);
+        try {
+            accountService.transfer("act-001", "act-002", 10000);
+            System.out.println("转账成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}

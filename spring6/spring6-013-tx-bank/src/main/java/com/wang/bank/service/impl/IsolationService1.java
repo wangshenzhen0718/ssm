d99d0e428ae46fd2288b1ca803572d60dcb0da00
wangshenzhen0718 <153474100@qq.com>
2022-12-08 16:54:45 +0800

    只读事务readOnly

--- spring6/spring6-013-tx-bank/src/main/java/com/wang/bank/service/impl/IsolationService1.java
+++ spring6/spring6-013-tx-bank/src/main/java/com/wang/bank/service/impl/IsolationService1.java
@@ -18,7 +18,10 @@ public class IsolationService1 {
     // 当前事务可以读取到别的事务没有提交的数据。
     //@Transactional(isolation = Isolation.READ_UNCOMMITTED)
     // 对方事务提交之后的数据我才能读取到。
-    @Transactional(isolation = Isolation.READ_UNCOMMITTED)
+    @Transactional(isolation = Isolation.READ_UNCOMMITTED, readOnly = true)
+    /*将当前事务设置为只读事务，在该事务执行过程中只允许select语句执行，delete insert update均不可执行。
+        该特性的作用是：启动spring的优化策略。提高select语句执行效率。
+        如果该事务中确实没有增删改操作，建议设置为只读事务。*/
     public void getByActno(String actno) {
         Account account = accountDao.selectByActno(actno);
         System.out.println("查询到的账户信息：" + account);
